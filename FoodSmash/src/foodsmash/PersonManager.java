db0dd7be2e97f1f3388bbed6173332e9dd27d3ea
TXAI\txai1 <txaipereira@gmail.com>
2018-06-07 14:09:32 +0200

    Edited PersonManager

--- FoodSmash/src/foodsmash/PersonManager.java
+++ FoodSmash/src/foodsmash/PersonManager.java
@@ -1,19 +1,37 @@
 package foodsmash;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Txai Pereira de Lacerda 50078467
  */
-public class PersonManager
-{
-    Person[] persons;  
-    public void Person(Person[] persons)
+public class PersonManager extends Person
+{   
+    List<Person> players = new ArrayList<>();
+    
+    public PersonManager(String username, int score, boolean isSelected) 
     {
-        for (Person person : persons) 
-        {
-            person.setUsername("");
-            person.setScore(0);
-            person.setIsSelected(false);
-        }
+        super(username, score, isSelected);
     }
+
+    public List<Person> getPlayers() 
+    {
+        return this.players;
+    }
+
+    public void addPlayer(Person player) 
+    {
+        this.players.add(player);
+    }    
+
+    public void updatePlayer(Person player) {
+        this.players.stream().filter(o ->  
+                o.getUsername().equals(player.getUsername()));
+    }
+    
+    
+    
+    
 }
